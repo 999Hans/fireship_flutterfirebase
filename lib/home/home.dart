4d60f1428da6b6b524926072d09390f89b742fba
999Hans <ohy0720@gmail.com>
2022-05-19 21:02:33 +0900

    add login ways

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 
+import '../login/login.dart';
+import '../services/auth.dart';
+import '../topics/topics.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () => Navigator.pushNamed(context, '/about'),
-            child: Text(
-              'about',
-              style: Theme.of(context).textTheme.bodyText1,
-            )),
-      ),
-    );
+    return StreamBuilder(
+        stream: AuthService().userStream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('waiting');
+          } else if (snapshot.hasError) {
+            return const Text('error');
+          } else if (snapshot.hasData) {
+            return const TopicsScreen();
+          } else {
+            return const LoginScreen();
+          }
+        });
   }
 }
